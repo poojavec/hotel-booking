0c86775fe00c6c3fb5fe4739b9a693c06075f6cd
poojavec <rpspooja2005@gmail.com>
2026-04-16 12:07:44 +0530

    hotelbooking

--- hotel-backend/booking/src/main/java/com/hotel/booking/HotelBookingApplication.java
+++ hotel-backend/booking/src/main/java/com/hotel/booking/HotelBookingApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BookingApplication {
+public class HotelBookingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BookingApplication.class, args);
+		SpringApplication.run(HotelBookingApplication.class, args);
 	}
 
 }
